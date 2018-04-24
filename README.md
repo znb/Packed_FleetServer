a2e5a769c6ffe2cc0fe2412b6cd4c6648e179bab
Matt <github@zonbi.org>
2018-04-24 18:26:57 +0100

    Initial commit - Fleet server

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Packed_FleetServer
-Build a simple Kolide Fleet server using Packer and Ansible
+# Fleet-Packer
+User Packer + Ansible to build a simple Kolide Fleet server
+
+This will use a basic Ubuntu 16.04 image (ami-996372fd) to build a simple Fleet server with OSQuery installed.
+
+It's incredibly hacky but gets the job done. It was mostly done so I could learn Packer and Ansible.
+
+Feedback welcome.
---
+++ provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+sudo apt -q update
+echo "apt-get update done."
+sudo DEBIAN_FRONTEND=noninteractive apt -qy -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"  upgrade
+sudo apt install -qy python-dev python-pip openssh-server language-pack-en
+sudo pip install --upgrade pip
+sudo pip uninstall pyOpenSSL
+sudo pip install pyOpenSSL==17.5.0
+sudo pip install ansible
+sudo timedatectl set-timezone Europe/London
+sudo localectl set-locale LANG=en_GB.utf8
+echo "Running build."
+sudo ansible-playbook /home/ubuntu/playbook.yaml
